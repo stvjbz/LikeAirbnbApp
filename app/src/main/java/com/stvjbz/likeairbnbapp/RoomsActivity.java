3980314f96dd79cfe0888a13f8badc6f5640ee70
stvjbz <stvjbz@outlook.com>
2016-08-29 00:11:24 +0900

    [modify] MainActivity layout.

--- app/src/main/java/com/stvjbz/likeairbnbapp/RoomsActivity.java
+++ app/src/main/java/com/stvjbz/likeairbnbapp/RoomsActivity.java
@@ -1,15 +1,21 @@
 package com.stvjbz.likeairbnbapp;
 
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v4.view.WindowCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class RoomsActivity extends AppCompatActivity {
 
@@ -29,12 +35,6 @@ public class RoomsActivity extends AppCompatActivity {
         // 背景を透明に。
         actionbarBackground.setAlpha(0);
 
-        title.setText(R.string.app_name);
-        title.setTextColor(Color.BLACK);
-        title.setGravity(Gravity.CENTER_VERTICAL);
-        // タイトルも透明にする
-        //title.setAlpha(0);
-
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setHideOnContentScrollEnabled(true);
@@ -42,6 +42,18 @@ public class RoomsActivity extends AppCompatActivity {
             actionBar.setBackgroundDrawable(actionbarBackground);
             actionBar.setCustomView(title);
             actionBar.setDisplayHomeAsUpEnabled(true);
+            //actionBar.setTitle("テスト・テスト");
+
+            // タイトル
+            String title = "宇都宮";
+            int titleColor = Color.BLACK;
+            String htmlColor = String.format(Locale.US, "#%06X", (0xFFFFFF & Color.argb(0, Color.red(titleColor), Color.green(titleColor), Color.blue(titleColor))));
+            String titleHtml = "<font color=\"" + htmlColor + "\">" + title + "</font>";
+            actionBar.setTitle(Html.fromHtml(titleHtml));
+            // アップボタン
+            final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+            upArrow.setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
+            actionBar.setHomeAsUpIndicator(upArrow);
         }
     }
 
